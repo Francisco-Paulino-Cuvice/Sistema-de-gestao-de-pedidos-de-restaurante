b35f11c58a5a6781597e88229633507f1d91bdbc
Francisco-Paulino-Cuvice <francisco.cuvice@hotmail.com>
2025-01-02 08:17:11 -0300

    Setando ClienteController e atualizando ClienteService

--- SGPR Backend/SGPR/src/main/java/bcc/SGPR/Controllers/ClienteController.java
+++ SGPR Backend/SGPR/src/main/java/bcc/SGPR/Controllers/ClienteController.java
@@ -1,9 +1,50 @@
 package bcc.SGPR.Controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import bcc.SGPR.Entities.Cliente;
+import bcc.SGPR.Entities.Pedido;
+import bcc.SGPR.Exceptions.ClienteNotFoundException;
+import bcc.SGPR.Services.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/cliente")
 @RestController
 public class ClienteController {
+    @Autowired
+    private ClienteService clienteService;
+
+    @PostMapping
+    public Cliente create(@RequestBody Cliente cliente) {
+        return clienteService.create(cliente);
+    }
+
+    @GetMapping
+    public List<Cliente> read() {
+        return clienteService.read();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> read(@PathVariable String id){
+        try{
+            Cliente cliente = this.clienteService.read(id);
+            return new ResponseEntity(cliente, HttpStatus.OK);
+        }catch (ClienteNotFoundException cnfe){
+            return new ResponseEntity(cnfe.getMessage(),HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente> update(@PathVariable String id, @RequestBody Cliente cliente){
+        Cliente clienteOriginal = this.clienteService.update(id, cliente);
+        return new ResponseEntity(clienteOriginal, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable String id) {
+        clienteService.delete(id);
+    }
 }
--- SGPR Backend/SGPR/src/main/java/bcc/SGPR/Services/ClienteService.java
+++ SGPR Backend/SGPR/src/main/java/bcc/SGPR/Services/ClienteService.java
@@ -1,11 +1,13 @@
 package bcc.SGPR.Services;
 
 import bcc.SGPR.Entities.Cliente;
+import bcc.SGPR.Exceptions.ClienteNotFoundException;
 import bcc.SGPR.Repositories.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ClienteService {
@@ -15,18 +17,38 @@ public class ClienteService {
 
 
     public Cliente create(Cliente cliente) {
-        return clienteRepository.save(cliente);
+        this.clienteRepository.save(cliente);
+        return cliente;
     }
 
     public List<Cliente> read() {
         return clienteRepository.findAll();
     }
 
-    public Cliente update(String id) {
-        return clienteRepository.findById(id).orElse(null);
+    public Cliente read(String id) throws ClienteNotFoundException {
+        Optional opt = this.clienteRepository.findById(id);
+        if(!opt.isPresent()){
+            throw new ClienteNotFoundException(id);
+        }
+        return (Cliente)opt.get();
+    }
+
+    public Cliente update(String id, Cliente cliente) throws ClienteNotFoundException{
+        Cliente clienteOriginal = read(id);
+        clienteOriginal.setClienteNome(cliente.getClienteNome());
+        clienteOriginal.setEmail(cliente.getEmail());
+        clienteOriginal.setEndereco(cliente.getEndereco());
+        clienteOriginal.setHistorico(cliente.getHistorico());
+        clienteOriginal.setTelefone(cliente.getTelefone());
+
+        this.clienteRepository.save(clienteOriginal);
+        return clienteOriginal;
     }
 
     public void delete(String id) {
-        clienteRepository.deleteById(id);
+        if(!this.clienteRepository.existsById(id)){
+            throw new ClienteNotFoundException(id);
+        }
+        this.clienteRepository.deleteById(id);
     }
 }
